c169b4f8c3eca7b11387b3df593b13015ca80ce1
Jesse Shaw <JesseShawCodes@gmail.com>
2023-04-04 23:10:11 -0400

    Firebase code, skeleton Login and Create Account Pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticleListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
+import LoginPage from './pages/LoginPage';
+import CreateAccountPage from './pages/CreateAccountPage';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import NotFoundPage from './pages/NotFoundPage';
@@ -19,6 +21,8 @@ function App() {
             <Route path="/about" element={<AboutPage />} />
             <Route path="/articles" element={<ArticleListPage />} />
             <Route path="/articles/:articleId" element={<ArticlePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/create_account" element={<CreateAccountPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAucVo1K5nZf9FdhjtECHtQC14DigXxAls",
+  authDomain: "my-blog-app-49f75.firebaseapp.com",
+  projectId: "my-blog-app-49f75",
+  storageBucket: "my-blog-app-49f75.appspot.com",
+  messagingSenderId: "842173225902",
+  appId: "1:842173225902:web:4f8b1eb90505122f7f463f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
