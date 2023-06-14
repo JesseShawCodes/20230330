8e0b8e11073dbb5e5039ca4d90fe1c3e8fa0dc85
Jesse Shaw <JesseShawCodes@gmail.com>
2023-06-14 10:00:57 -0400

    Store cleanup

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './features/counter/couterSlice'
 
-import { setupListeners } from '@reduxjs/toolkit/dist/query'
 import { pokemonApi } from './services/pokemon'
 
 export const store = configureStore({
