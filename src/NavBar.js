2c516aee065643ba3f7f8dde6c8dafdfb86f3bf9
Jesse Shaw <JesseShawCodes@gmail.com>
2023-07-08 11:16:57 -0400

    Updated Name in Nav Bar

--- src/NavBar.js
+++ src/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <Link to="/" className='nav-link active navbar-brand'>Blog It</Link>
+                <Link to="/" className='nav-link active navbar-brand'>Spotify App</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
